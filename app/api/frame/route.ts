939abe201f4167d0c58b11b6d78a0a933109d207
paulaamstutz <paula@saynode.ch>
2024-03-01 12:30:43 +0100

    try8

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -15,25 +15,24 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/question_1.png" />
         <meta property="fc:frame:button:1" content="Option 1" />
-        <meta property="fc:frame:button:1:action" content="https://frames-js-sable.vercel.app/api/frame?id=2" />
+        <meta property="fc:frame:button:1:action" content="/api/frame?id=2" />
         <meta property="fc:frame:button:2" content="Option 2" />
-        <meta property="fc:frame:button:2:action" content="https://frames-js-sable.vercel.app/api/frame?id=2" />
+        <meta property="fc:frame:button:2:action" content="/api/frame?id=2" />
         <meta property="fc:frame:button:3" content="Option 3" />
-        <meta property="fc:frame:button:3:action" content="https://frames-js-sable.vercel.app/api/frame?id=3" />
-        <meta property="fc:frame:post_url" content="https://frames-js-sable.vercel.app" />
+        <meta property="fc:frame:button:3:action" content="/api/frame?id=3" />
+        <meta property="fc:frame:post_url" content="/api/frame?id=${nextId}" />
       </head></html>`);
 
-    case 2:
-      // Frame 2 with a Subscribe button
-      return new NextResponse(`<!DOCTYPE html><html><head>
-        <title>This is frame 2</title>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png" />
-        <meta property="fc:frame:button:1" content="Subscribe" />
-        <meta property="fc:frame:button:1:action" content="https://warpcast.com/skillbuddy" />
-        <meta property="fc:frame:post_url" content="https://frames-js-sable.vercel.app" />
-      </head></html>`);
-
+      case 2:
+        // Frame 2 with a Subscribe button
+        return new NextResponse(`<!DOCTYPE html><html><head>
+          <title>This is frame 2</title>
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png" />
+          <meta property="fc:frame:button:1" content="Subscribe" />
+          <meta property="fc:frame:button:1:action" content="https://warpcast.com/skillbuddy" />
+          <meta property="fc:frame:post_url" content="/api/frame?id=${nextId}" />
+        </head></html>`);
 
     case 4:
       // Frame 3 with a Collect Reward button
@@ -43,7 +42,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png" />
         <meta property="fc:frame:button:1" content="Collect Your Reward" />
         <meta property="fc:frame:button:1:action" content="https://warpcast.com/skillbuddy" />
-        <meta property="fc:frame:post_url" content="https://frames-js-sable.vercel.app" />
+        <meta property="fc:frame:post_url" content="/api/frame?id=${nextId}" />
       </head></html>`);
 
     default:
@@ -53,8 +52,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png" />
         <meta property="fc:frame:button:1" content="Subscribe" />
-        <meta property="fc:frame:button:1:action" content="https://warpcast.com/skillbuddy" />
-        <meta property="fc:frame:post_url" content="https://warpcast.com/skillbuddy" />
+        <meta property="fc:frame:button:1:action" content="https://warpcast.com/skillbuddy"" />
+        <meta property="fc:frame:post_url" content="/api/frame?id=${nextId}" />
       </head></html>`);
   }
 }
