cbde7e086dfd3094b0e08d9079bf6aff1ee93d48
paulaamstutz <paula@saynode.ch>
2024-03-01 13:36:10 +0100

    try12

--- app/api/frame/[id].tsx
+++ app/api/frame/[id].tsx
@@ -13,11 +13,11 @@ export default async function handler(req: NextRequest): Promise<NextResponse> {
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="https://frame-warpcast.s3.eu-central-1.amazonaws.com/question_1.png" />
       <meta property="fc:frame:button:1" content="Option 1" />
-      <meta property="fc:frame:button:1:action" content="https://frames-js-sable.vercel.app/api/frame?id=2}" />
+      <meta property="fc:frame:button:1:action" content="https://frames-js-sable.vercel.app/api/frame?id=2" />
       <meta property="fc:frame:button:2" content="Option 2" />
       <meta property="fc:frame:button:2:action" content="https://frames-js-sable.vercel.app/api/frame?id=2" />
       <meta property="fc:frame:button:3" content="Option 3" />
-      <meta property="fc:frame:button:3:action" content="https://frames-js-sable.vercel.app/api/frame?id=3}" />
+      <meta property="fc:frame:button:3:action" content="https://frames-js-sable.vercel.app/api/frame?id=3" />
       <meta property="fc:frame:post_url" content="https://frames-js-sable.vercel.app" />
     </head></html>`);
     } else if (idAsNumber === 2) {
