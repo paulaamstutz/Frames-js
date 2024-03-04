2bda983e0ebb70d8bfcaa3541fac0b33256c689b
paulaamstutz <paula@saynode.ch>
2024-03-04 15:34:41 +0100

    try16

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,26 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
-
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: "First Choice",
-    },
-    {
-      label: "Second Choice",
-    },
-    {
-      label: "Third Choice",
-    }
-  ],
-  image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
-  post_url: `https://frames-js-sable.vercel.app/api/frame/1`,
-});
-
-export const metadata: Metadata = {
-  title: 'Cosmic Cowboys',
-  description: 'A frame telling the story of Cosmic Cowboys',
-  openGraph: {
-    title: 'Cosmic Cowboys',
-    description: 'A frame telling the story of Cosmic Cowboys',
-    images: ["https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png"],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+import { useRouter } from 'next/router';
 
 export default function Page() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: "First Choice",
+      },
+      {
+        label: "Second Choice",
+      },
+      {
+        label: "Third Choice",
+      }
+    ],
+    image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
+    post_url: `https://frames-js-sable.vercel.app/api/frame/${id}`,
+  });
+
   return (
     <>
       <h1>SkillBuddy</h1>
