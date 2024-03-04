c97921ad18a6abe0c9dc3ee38bfeb71eeb0fcd6e
paulaamstutz <paula@saynode.ch>
2024-03-04 15:59:00 +0100

    try15

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,20 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: "First Choice",
-    },
-    {
-      label: "Second Choice",
-    },
-    {
-      label: "Third Choice",
-    }
-  ],
-  image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
-  post_url: `https://frames-js-sable.vercel.app/api/frame/`,
-});
+export default function Page({ id }: { id: any }) {
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      {
+        label: "First Choice",
+      },
+      {
+        label: "Second Choice",
+      },
+      {
+        label: "Third Choice",
+      }
+    ],
+    image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
+    post_url: `https://frames-js-sable.vercel.app/api/frame/${id}`,
+  });
 
-export const metadata: Metadata = {
-  title: 'SkillBuddy',
-  openGraph: {
-    title: 'SkillBuddy',
-    images: ["https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png"],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
-
-export default function Page() {
-  return (
-    <>
-      <h1>SkillBuddy</h1>
-    </>
-  );
 }
