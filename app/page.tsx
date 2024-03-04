c8b690a2faab2277a2854520600eae906f16da7a
paulaamstutz <paula@saynode.ch>
2024-03-04 15:58:32 +0100

    try14

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,34 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
-import { useRouter } from 'next/router';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "First Choice",
+    },
+    {
+      label: "Second Choice",
+    },
+    {
+      label: "Third Choice",
+    }
+  ],
+  image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
+  post_url: `https://frames-js-sable.vercel.app/api/frame/`,
+});
+
+export const metadata: Metadata = {
+  title: 'SkillBuddy',
+  openGraph: {
+    title: 'SkillBuddy',
+    images: ["https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png"],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
 export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const frameMetadata = getFrameMetadata({
-    buttons: [
-      {
-        label: "First Choice",
-      },
-      {
-        label: "Second Choice",
-      },
-      {
-        label: "Third Choice",
-      }
-    ],
-    image: "https://frame-warpcast.s3.eu-central-1.amazonaws.com/skillbuddy_canva.png",
-    post_url: `https://frames-js-sable.vercel.app/api/frame/${id}`,
-  });
-
   return (
     <>
       <h1>SkillBuddy</h1>
